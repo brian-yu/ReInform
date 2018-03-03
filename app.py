26445f7cc4474333aba8d807c427595fb8e8affc
Brian Yu <brian@byu.io>
2018-03-03 15:52:58 -0500

    heh

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template, url_for
+from flask_cors import CORS
+import requests
+import xmltodict, json
+
+app = Flask(__name__, static_url_path='')
+CORS(app)
+
+
+OPEN_SECRETS_KEY = "40411c191fd58f5709214a9184c9ca1d";
+
+@app.route("/")
+def index():
+    return app.send_static_file('index.html')
+
+@app.route("/state/<state>")
+def getLegislators(state):
+	url = "http://www.opensecrets.org/api/?method=getLegislators&id=" + state + "&apikey=" + OPEN_SECRETS_KEY
+	r = requests.get(url)
+
+	o = xmltodict.parse(r.text)['response']['legislator']
+	return json.dumps(o) # '{"e": {"a": ["text", "text"]}}'
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Setup
+
+1. install python 3
+2. install deps
+`pip install flask
+pip install flask_cors
+pip install requests
+pip install xmltodict`
+
+
