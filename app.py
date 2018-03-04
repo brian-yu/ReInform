4fe310d76331849f307f58fc1ea1fa207d7901d4
tsingh2017 <tsinghqs@vt.edu>
2018-03-04 01:22:58 -0500

    sugoi

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, url_for
 from flask_cors import CORS
 import requests
 import xmltodict, json
+from collections import OrderedDict
 
 app = Flask(__name__, static_url_path='')
 CORS(app)
@@ -11,7 +12,7 @@ OPEN_SECRETS_KEY = "40411c191fd58f5709214a9184c9ca1d";
 
 @app.route("/")
 def index():
-    return app.send_static_file('index.html')
+	return app.send_static_file('index.html')
 
 @app.route("/state/<state>")
 def getLegislators(state):
@@ -21,7 +22,76 @@ def getLegislators(state):
 	o = xmltodict.parse(r.text)['response']['legislator']
 	return json.dumps(o) # '{"e": {"a": ["text", "text"]}}'
 
-@app.route("/id/<state>")
-def candidContrib(cid, cycle):
-	url = "https://www.opensecrets.org/api/?method=candContrib&cid" + cid + "cycle=2018&apikey=" + OPEN_SECRETS_KEY+ "&output=json"
-	return json.dumps(requests.get(url))
+#function to get the candidate funding accross four years
+@app.route("/id/<cid>")
+def candidContrib(cid):
+#get our url data across the four seperate years
+	url2012 = "https://www.opensecrets.org/api/?method=candContrib&cid=" + cid + "&cycle=2012&apikey=" + OPEN_SECRETS_KEY+ "&output=xml"
+	url2014 = "https://www.opensecrets.org/api/?method=candContrib&cid=" + cid + "&cycle=2014&apikey=" + OPEN_SECRETS_KEY+ "&output=xml"
+	url2016 = "https://www.opensecrets.org/api/?method=candContrib&cid=" + cid + "&cycle=2016&apikey=" + OPEN_SECRETS_KEY+ "&output=xml"
+	url2018 = "https://www.opensecrets.org/api/?method=candContrib&cid=" + cid + "&cycle=2018&apikey=" + OPEN_SECRETS_KEY+ "&output=xml"
+#get our url requests
+	r1 = requests.get(url2012)
+	r2 = requests.get(url2014)
+	r3 = requests.get(url2016)
+	r4 = requests.get(url2018)
+
+#Create our dictionaries by parsing the xml
+	o1 = xmltodict.parse(r1.text)['response']
+	o2 = xmltodict.parse(r2.text)['response']
+	o3 = xmltodict.parse(r3.text)['response']
+	o4 = xmltodict.parse(r4.text)['response']
+
+	# o1 = json.dumps(o1)
+
+
+	import pprint
+
+	pp = pprint.PrettyPrinter(indent=4)
+
+	funding = dict()
+	length = len(o1['contributors'])
+	for x in range(0, length):
+		funding[str(o1['contributors']['contributor'][x]['@org_name'])] = int(o1['contributors']['contributor'][x]['@total'])
+	length2 = len(o2['contributors'])
+
+	for x in range(0, length2):
+		if o2['contributors']["contributor"][x]['@org_name'] in funding.keys():
+			funding[str(o2['contributors']['contributor'][x]['@org_name'])] = funding.get(o2['contributors']["contributor"][x]['@org_name']) + int(o2['contributors']['contributor'][x]['@total'])
+		else:
+			funding[str(o2['contributors']['contributor'][x]['@org_name'])] = int(o1['contributors']['contributor'][x]['@total'])
+	length3 = len(o3['contributors'])
+	for x in range(0, length3):
+		if o3['contributors']["contributor"][x]['@org_name'] in funding.keys():
+			funding[str(o3['contributors']['contributor'][x]['@org_name'])] = funding.get(o3['contributors']["contributor"][x]['@org_name']) + int(o3['contributors']['contributor'][x]['@total'])
+		else:
+			funding[str(o3['contributors']['contributor'][x]['@org_name'])] = int(o3['contributors']['contributor'][x]['@total'])
+	length4 = len(o4['contributors'])
+	for x in range(0, length3):
+		if o4['contributors']["contributor"][x]['@org_name'] in funding.keys():
+			funding[str(o4['contributors']['contributor'][x]['@org_name'])] = funding.get(o4['contributors']["contributor"][x]['@org_name']) + int(o4['contributors']['contributor'][x]['@total'])
+		else:
+			funding[str(o4['contributors']['contributor'][x]['@org_name'])] = int(o4['contributors']['contributor'][x]['@total'])
+	
+	#funding = sorted(funding.values())
+	ans = dict()
+	for x in range(0, 5):
+		max = 0
+		for key in funding:
+			if funding[key] > max:
+				max = funding[key]
+				check = key
+		ans[check] = max
+		del funding[check]
+		
+
+	# 
+	# funding[str(o1['contributors']['contributor'][0]['@org_name'])] = str(o1['contributors']['contributor'][0]['@total'])
+	# funding[str(o1['contributors']['contributor'][1]['@org_name'])] = str(o1['contributors']['contributor'][1]['@total'])
+	# funding[str(o1['contributors']['contributor'][2]['@org_name'])] = str(o1['contributors']['contributor'][2]['@total'])
+	# funding[str(o1['contributors']['contributor'][3]['@org_name'])] = str(o1['contributors']['contributor'][3]['@total'])
+	# funding[str(o1['contributors']['contributor'][4]['@org_name'])] = str(o1['contributors']['contributor'][4]['@total'])
+	
+	answer = json.dumps(ans)
+	return answer
+	#['contributors']['contributor'][0]['@org_name']) + str()
