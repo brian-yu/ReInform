d531d3bf9559de689c4554c5e1caa921eaf372ba
Brian Yu <brian@byu.io>
2018-03-09 18:12:15 -0500

    State sidebar - NEEDS FIX

---
+++ app/src/components/LoadingAnimation.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class LoadingAnimation extends Component {
+  render() {
+    return (
+	    <div id="loading" className="spinner">
+	      <div className="rect1"></div>
+	      <div className="rect2"></div>
+	      <div className="rect3"></div>
+	      <div className="rect4"></div>
+	      <div className="rect5"></div>
+	    </div>
+	);
+  }
+}
+
+export default LoadingAnimation;
--- app/src/components/State.js
+++ app/src/components/State.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import stateAbbrevs from '../data/stateAbbrevs';
+import LoadingAnimation from './LoadingAnimation';
 
 class State extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      legislators: null,
+    }
   }
 
-  componentDidMount() {
+  componentDidUpdate() {
+    // this.setState({
+    //   legislators: null,
+    // })
+  }
+
+  Legislators = () => {
+    // const abbrev = stateAbbrevs[this.props.currState];
+    // axios.get('/state/' + abbrev).then((response) => {
+    //   this.setState({
+    //     legislators: response.data.response.legislator
+    //   })
+    // });
+    // console.log(this.state);
+    if (this.state.legislators == null) {
+      return (
+        <LoadingAnimation/>
+      );
+    } else {
+      return null;
+    }
   }
 
   render() {
@@ -15,7 +41,7 @@ class State extends Component {
       <div id="sidebar">
         <h2 id="sidebar-title">{this.props.currState}</h2>
         <div id="sidebar-body" className="scrollbar style-1">
-          
+          <this.Legislators />
         </div>
       </div>
     );
